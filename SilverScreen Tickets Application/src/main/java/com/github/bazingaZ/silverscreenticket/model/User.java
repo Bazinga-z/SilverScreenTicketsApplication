fbb13794cafa595102ef37a66c251d79337ebc8b
Maryam <maryam.sadat.jafary79@gmail.com>
2025-05-03 13:24:12 +0330

    "fix: update User.java before merging the test branch to main"

--- SilverScreen Tickets Application/src/main/java/com/github/bazingaZ/silverscreenticket/model/User.java
+++ SilverScreen Tickets Application/src/main/java/com/github/bazingaZ/silverscreenticket/model/User.java
@@ -1,4 +1,47 @@
 package com.github.bazingaZ.silverscreenticket.model;
 
 public class User {
+    private String name;
+    private int phoneNumber;
+    private String email;
+    private Ticket ticket;
+
+    public User(String name, int phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Ticket getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(Ticket ticket) {
+        this.ticket = ticket;
+    }
+
+
 }
